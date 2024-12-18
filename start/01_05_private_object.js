0d33469ccfbe200438dbe89f8d87c364580da277
Caleb Hamilton <caleb542@users.noreply.github.com>
2024-12-18 19:32:32 +0000

    01_04 added

--- start/01_05_private_object.js
+++ start/01_05_private_object.js
@@ -1,1 +1,12 @@
 // Write your code here
+const username = Symbol("username")
+const password = Symbol("password")
+
+const user = {
+  [username]: "Caleb The Hammer Hamilton",
+  [password]: "123456",
+  age:27
+}
+
+console.log(user.username);
+console.log(user.password);
