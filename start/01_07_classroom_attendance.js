05636c3921b23634ee2489bad363ad936635aa37
Caleb Hamilton <caleb542@users.noreply.github.com>
2024-12-20 17:13:03 +0000

    _1_08 added

--- start/01_07_classroom_attendance.js
+++ start/01_07_classroom_attendance.js
@@ -1,1 +1,22 @@
-// Write your code here
+/* Destructuring Arrays */
+
+
+
+
+ function getStudents(classroom) {
+  let {hasTeachingAssistant, classlist} = classroom;
+  let teacher, ta, students;
+
+   if( hasTeachingAssistant ){
+     [teacher, ta, ...students] = classlist;
+   } else {
+      [teacher, ...students] = classlist;
+   }
+   return students
+  }
+
+console.log( getStudents({
+  hasTeachingAssistant: false,
+  classlist : ["Rashida", "John", "Roman", "Lisa", "Omair", "Lukas"]
+})
+)
