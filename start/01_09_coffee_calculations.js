b05688cd939bd22f569318cdb79ab5084eefd6b0
Caleb Hamilton <caleb542@users.noreply.github.com>
2024-12-21 16:13:55 +0000

    adding 01_09_coffee...

--- start/01_09_coffee_calculations.js
+++ start/01_09_coffee_calculations.js
@@ -1,2 +1,15 @@
-// Write your code here
-console.log('Hi Hello, Woo Hoo')
+const array = [3.00,1.25,67,12];
+
+function coffeeDate(){
+  let totalOrders = array.reduce( 
+    (acc, cur) => acc + cur, initialValue); 
+  // let total = totalOrders * price
+  return `The total bill for the coffees was $${totalOrders}`
+}
+const initialValue = 0;
+const price = 1.25;
+
+
+
+console.log(coffeeDate(array))
+
